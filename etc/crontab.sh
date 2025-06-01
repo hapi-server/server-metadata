9c1fd7f1ad4068cc09d0b50496cb95d5149c58f9
Bob Weigel <rweigel@gmu.edu>
2025-06-01 02:04:17 -0400

    update crontab

--- etc/crontab.sh
+++ etc/crontab.sh
@@ -1,4 +1,6 @@
-# Execute from ../
+# Execute from ../ using /bin/zsh -i etc/crontab.sh
+# The -i is needed to force it to read .zshrc, which sets conda env
+
 TEE=2>&1 | tee etc/log/crontab-$(date +%Y-%m-%d).log
 
 if [ ! -d "servers" ]; then
@@ -7,11 +9,12 @@ else
   git -C servers pull --rebase $TEE
 fi
 
+python --version
 pip install -e . $TEE
 
 python abouts.py
-git -C servers commit -a -m "Update abouts.json, all.txt, all_.txt [skip ci]" >> $TEE
-git  -C servers push $TEE
+#git -C servers commit -a -m "Update abouts.json, all.txt, all_.txt [skip ci]" >> $TEE
+#git  -C servers push $TEE
 
-python catalogs.py
-python availabilities.py
+#python catalogs.py
+#python availabilities.py
