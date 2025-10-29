c74f5970f0a65c704d55d3435c3fab122e1ba2aa
Bob Weigel <rweigel@gmu.edu>
2025-10-29 12:20:49 -0400

    finish table.py

--- table.py
+++ table.py
@@ -1,53 +1,70 @@
-# Incomplete and not yet used.
 import utilrsw
 import tableui
 
+def reorder_keys(d):
+  # move keys starting with 'x_' to the end, preserving relative order
+  x_items = [(k, d[k]) for k in d.keys() if k.startswith('x_')]
+  other_items = [(k, d[k]) for k in d.keys() if not k.startswith('x_')]
+  newd = {}
+  for k, v in other_items:
+    newd[k] = v
+  for k, v in x_items:
+    newd[k] = v
+  return newd
+
+def compute_rows(all_file, omits=[]):
+  servers = utilrsw.read(all_file)
+  rows = {'dataset': [], 'parameter': []}
+  for server in servers:
+    catalog = servers[server]['catalog']
+    for i, dataset in enumerate(catalog):
+      dataset['server'] = server
+      for omit in omits:
+        utilrsw.rm_path(dataset, omit, ignore_error=True)
+        utilrsw.rm_path(dataset, omit, ignore_error=True)
+
+      if utilrsw.get_path(dataset, ['info', 'parameters']) is not None:
+        dataset['NumParameters'] = len(dataset['info']['parameters'])
+        for parameter in dataset['info']['parameters']:
+          parameter = {"server": server, "id": dataset['id'], **parameter}
+          row = utilrsw.flatten_dicts(parameter, simplify=True)
+          rows['parameter'].append(reorder_keys(row))
+
+      row = utilrsw.flatten_dicts(dataset, simplify=True)
+      rows['dataset'].append(reorder_keys(row))
+  return rows
+
 config = {
-  "use_all_attributes": True,
-  "path_type": "list",
-  "omit_attributes": ["x_customRequestOptions"],
-  "paths": {
-    "/": {
-      "server": None,
-      "id": None
+  "dataset": {
+    "name": 'hapi.all.datasets',
+    "out_dir": 'data/table',
+    "use_all_attributes": True,
+    "path_type": "list",
+    "omit_attributes": [
+      "x_customRequestOptions",
+      "parameters"
+    ],
+    "paths": {
+      "/": {
+        "server": None,
+        "id": None
+      }
+    }
+  },
+  "parameter": {
+    "name": 'hapi.all.parameters',
+    "out_dir": 'data/table',
+    "use_all_attributes": True,
+    "paths": {
+      "/": {
+        "server": None,
+        "id": None,
+        "name": None
+      }
     }
   }
 }
-
-servers = utilrsw.read('data/catalogs-all.pkl')
-datasets_combined = []
-catalogs = {}
-parameters = {}
-for server in servers:
-  catalogs[server] = servers[server]['catalog']
-  for i, dataset in enumerate(catalogs[server]):
-    print(f"  {dataset['id']}")
-    dataset['server'] = server
-    if utilrsw.get_path(dataset, ['info']) is not None:
-      del dataset['info']['HAPI']
-      del dataset['info']['status']
-    if utilrsw.get_path(dataset, ['info', 'parameters']) is not None:
-      dataset['NumParameters'] = len(dataset['info']['parameters'])
-      parameters[f"{server}{i}"] = dataset['info']['parameters'].copy()
-      for p in parameters[f"{server}{i}"]:
-        p['server'] = server
-        p['id'] = dataset['id']
-      del dataset['info']['parameters']
-    catalogs[server][i] = utilrsw.flatten_dicts(dataset, simplify=True)
-
-#utilrsw.print_dict(catalogs)
-info = tableui.dict2sql(catalogs, config, 'data/table/hapi-all-datasets', out_dir='.')
-#utilrsw.print_dict(info)
-config = {
-  "use_all_attributes": True,
-  "path_type": "list",
-  "paths": {
-    "/": {
-      "server": None,
-      "id": None,
-      "name": None
-    }
-  }
-}
-
-info = tableui.dict2sql(parameters, config, 'data/table/hapi-all-parameters', out_dir='.')
+omits = [['info', 'HAPI'], ['info', 'status']]
+rows = compute_rows('data/catalogs-all.pkl', omits=omits)
+tableui.dict2sql(rows['dataset'], config['dataset'])
+tableui.dict2sql(rows['parameter'], config['parameter'])
