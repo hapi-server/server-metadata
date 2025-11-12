254059ea1f1d58f5164dac03bf1e270e19c42f9f
Bob Weigel <rweigel@gmu.edu>
2025-11-12 09:01:11 -0500

    server admin

--- etc/serve-fs.py
+++ etc/serve-fs.py
@@ -2,7 +2,6 @@ import os
 import uvicorn
 import utilrsw
 
-if __name__ == "__main__":
-  root = os.path.join(utilrsw.script_info()['dir'], "..", "data")
-  app = utilrsw.servefs(root=root)
-  uvicorn.run(app, host="0.0.0.0", port=6001, server_header=False)
+root = os.path.join(os.path.abspath(__file__), "..", "data")
+app = utilrsw.servefs(root=root)
+uvicorn.run(app, host="0.0.0.0", port=6001, server_header=False)
--- etc/serve-table.py
+++
@@ -1,6 +0,0 @@
-import os
-import tableui
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-config = os.path.join(script_dir, "..", "table", "tableui.json")
-tableui.serve(config=config, port=6002)
