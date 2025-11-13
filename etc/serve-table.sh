8e40adc37bcc70895a55ea40f824cde1ee730fb7
Bob Weigel <rweigel@gmu.edu>
2025-11-12 19:59:14 -0500

    etc/server start-up

--- etc/serve-table.sh
+++ etc/serve-table.sh
@@ -1,3 +1,3 @@
 source /Users/weigel/anaconda3/etc/profile.d/conda.sh; conda activate
 conda activate python3.10.9-cdawmeta
-CONFIG="../table/tableui.json" uvicorn tableui:factory --factory --workers 2 --port 6002
+CONFIG="../table/tableui.json" uvicorn tableui:factory --factory --workers 4 --port 8052 --host 0.0.0.0
