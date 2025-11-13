e57f417efba0e06a4ca78f4af3195993a13e8ac9
Bob Weigel <rweigel@gmu.edu>
2025-11-13 10:41:14 -0500

    servefs

--- etc/serve-fs.py
+++ etc/serve-fs.py
@@ -2,6 +2,6 @@ import os
 import uvicorn
 import utilrsw
 
-root = os.path.join(os.path.abspath(__file__), "..", "data")
+root = os.path.join(utilrsw.script_info()['dir'], "..", "data")
 app = utilrsw.servefs(root=root)
 uvicorn.run(app, host="0.0.0.0", port=6001, server_header=False)
