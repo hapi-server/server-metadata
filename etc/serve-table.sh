0609b272c41bc4d22853ae165a08ab6e31fb87fe
Bob Weigel <rweigel@gmu.edu>
2025-11-18 13:15:29 -0500

    sh

--- etc/serve-table.sh
+++ etc/serve-table.sh
@@ -1,3 +1,3 @@
 source /Users/weigel/anaconda3/etc/profile.d/conda.sh; conda activate
 conda activate python3.10.9-cdawmeta
-python ../../table-ui/serve.py --config ../table/tableui.json --host 0.0.0.0 --port 6001 --workers 4
+python ../../table-ui/serve.py --config ../table/tableui.json --host 0.0.0.0 --port 8052 --workers 4
