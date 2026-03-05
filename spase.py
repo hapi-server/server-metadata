685ca2a8e0bd4a3aaa0842b8dfde553cf341630c
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-03-05 05:56:59 -0500

    SPASE/HAPI telecon updates

--- spase.py
+++ spase.py
@@ -45,13 +45,10 @@ def add_Parameter(Spase, dataset, map):
       Parameter = utilrsw.map_dict(parameter, map)
       Parameters.append(Parameter)
     Spase['NumericalData']['Parameter'] = Parameters
-  return None
 
 
-def add_AccessInformation(Spase, dataset, about, template):
+def add_AccessInformation(Spase, dataset, about, capabilities, formatMap, template):
   import copy
-
-  # TODO: Need to get formats supported from capabilities response.
 
   def script_info():
 
@@ -72,47 +69,91 @@ def add_AccessInformation(Spase, dataset, about, template):
     language_formats = []
     for language in languages.split(', '):
       language_formats.append(f"x_Script.{language.strip()}")
+
     return languages, language_formats
 
-  languages, formats = script_info()
+  def formats(capabilities):
+    outputFormats = utilrsw.get_path(capabilities, 'outputFormats', default=['csv'])
+
+    Formats = []
+    for fmt in outputFormats:
+      if fmt not in formatMap:
+        #log.warning(f"Unknown output format: {fmt}. Skipping.")
+        continue
+      Formats.append(config['formatMap'][fmt])
+    return Formats
+
+  def description(about):
+
+    def extra(type):
+      desc = ""
+      type_capitalized = type.capitalize()
+      note = utilrsw.get_path(about, type)
+      if note is not None:
+        if isinstance(note, str):
+          desc = f"{type_capitalized}: {note}"
+        if isinstance(note, list):
+          notes = ""
+          for i, n in enumerate(note):
+            notes += f"{i+1}. {n}; "
+          notes = notes.rstrip('; ')
+          desc = f"{type_capitalized}s: {notes}"
+      return desc
+
+    desc = ""
+    description = utilrsw.get_path(about, 'description')
+    contact = utilrsw.get_path(about, 'contact')
+    contactID = utilrsw.get_path(about, 'contactID')
+
+    if description is None and contact is None and contactID is None:
+      return None
+    if description is not None:
+      desc += description
+
+    if contact and contactID:
+      if contact == contactID:
+        desc = f"{desc} (Contact: {contact})"
+      else:
+        desc = f"{desc} (Contact: {contact} <{contactID}>)"
+    if not contact and contactID:
+      desc = f"{desc} (Contact: <{contactID}>)"
+    if contact and not contactID:
+      desc = f"{desc} (Contact: {contact})"
+
+    serverCitation = utilrsw.get_path(about, 'serverCitation')
+    if serverCitation is not None:
+      desc = f"{desc}. Server Citation: {serverCitation} (see dataset description for citing the dataset)"
+
+    note = extra('note')
+    if note:
+      desc = f"{desc}. {note}"
+
+    warning = extra('warning')
+    if warning:
+      desc = f"{desc}. {warning}"
+
+    return desc + "."
+
+  data_formats = formats(capabilities)
+
+  languages, language_formats = script_info()
 
   template = copy.deepcopy(template)
   for i in range(len(template)):
-    template[i]['AccessURL']['URL'] = template[i]['AccessURL']['URL'].format(server=dataset['server'], dataset=dataset['id'])
+    url = template[i]['AccessURL']['URL'].format(server=dataset['server'], dataset=dataset['id'])
+    template[i]['AccessURL']['URL'] = url
     template[i]['AccessURL']['ProductKey'] = dataset['id']
 
-  template[1]['AccessURL']['Description'] = template[1]['AccessURL']['Description'].format(languages=languages)
-  template[1]['AccessURL']['Format'] = formats
+  desc = description(about)
+  if desc is not None:
+    template[0]['AccessURL']['Description'] = desc
+  template[0]['AccessURL']['Format'] = data_formats
+
+  desc = template[1]['AccessURL']['Description'].format(languages=languages)
+  template[1]['AccessURL']['Description'] = desc
+  template[1]['AccessURL']['Format'] = language_formats
+
   Spase['NumericalData']['AccessInformation'] = template
-  return None
-
-
-def add_DOI(Spase, dataset):
-
-  def extract_doi(doi_string):
-    DOI = None
-    if doi_string.startswith('https://doi.org/'):
-      DOI = doi_string[len('https://doi.org/'):]
-    if doi_string.startswith('doi:'):
-      DOI = doi_string[len('doi:'):]
-    return DOI
-
-  #dataset['info']['datasetCitation'] = 'doi:10.1234/dataset1'
-
-  datasetCitation = utilrsw.get_path(dataset, 'info/datasetCitation', sep='/')
-  resourceID = utilrsw.get_path(dataset, 'info/resourceID', sep='/')
-
-  if datasetCitation is not None:
-    DOI = extract_doi(datasetCitation)
-    if DOI is not None:
-      Spase['NumericalData']['DOI'] = DOI
-  else:
-    if resourceID is not None:
-      DOI = extract_doi(resourceID)
-      if DOI is not None:
-        Spase['NumericalData']['DOI'] = DOI
-
-  return None
 
 
 def add_SpatialMapping(Spase, dataset):
@@ -138,11 +179,93 @@ def add_SpatialMapping(Spase, dataset):
         "centerLongitude": point[0],
         "centerLatitude": point[1]
       }
-    if len(point) > 2:
-      Spase['NumericalData']['SpatialMapping']['centerElevation'] = point[2]
+      if len(point) > 2:
+        Spase['NumericalData']['SpatialMapping']['centerElevation'] = point[2]
 
-  return None
 
+def add_ResourceHeader(Spase, dataset):
+
+  def extract_doi(doi_string):
+    DOI = None
+    if doi_string.startswith('https://doi.org/'):
+      DOI = doi_string[len('https://doi.org/'):]
+    if doi_string.startswith('doi:'):
+      DOI = doi_string[len('doi:'):]
+    return DOI
+
+  def informationURLs(dataset):
+    url_template = "https://hapi-server.org/servers/#server={server}&dataset={dataset}"
+    url = url_template.format(server=dataset['server'], dataset=dataset['id'])
+    InformationURLs = [{
+      "Name": "hapi-server.org",
+      "URL": url,
+      "Description": "hapi-server.org dataset information page"
+    }]
+
+    additionalMetadata = utilrsw.get_path(dataset, 'info.additionalMetadata')
+    if additionalMetadata is not None:
+      if isinstance(additionalMetadata, dict):
+        #https://github.com/hapi-server/data-specification/issues/282
+        additionalMetadata = [additionalMetadata]
+      for additional in additionalMetadata:
+
+        InformationURL = {"Name": "", "URL": ""}
+        desc = ""
+
+        if 'contentURL' not in additional and 'content' not in additional:
+          log.error(f"{dataset['id']}: additionalMetadata entry missing one of 'contentURL' or 'content'")
+          continue
+
+        if 'contentURL' in additional:
+          InformationURL["URL"] = additional['contentURL']
+
+        if 'content' in additional:
+          url = f"{dataset['server_url']}/info?dataset={dataset['id']}"
+          desc = f"Additional metadata content is available in the additionalMetadata node in: {url}"
+
+        if 'name' in additional:
+          InformationURL["Name"] = additional['name']
+        else:
+          InformationURL['Name'] = "Additional metadata"
+
+        if 'aboutURL' in additional:
+          desc += f"Metadata description: {additional['aboutURL']}"
+        if 'schemaURL' in additional:
+          desc += f". Metadata schema: {additional['schemaURL']}."
+
+        note = "The information in this InformationURL node is derived from an additionalMetadata node in HAPI /info response"
+        if desc != "":
+          InformationURL['Description'] = f"{desc}. {note}"
+        else:
+          InformationURL['Description'] = note
+
+        InformationURLs.append(InformationURL)
+
+    return InformationURLs
+
+  Spase['NumericalData']['ResourceHeader']["InformationURL"] = informationURLs(dataset)
+
+  #dataset['info']['datasetCitation'] = 'doi:10.1234/dataset1'
+
+  datasetCitation = utilrsw.get_path(dataset, 'info/datasetCitation', sep='/')
+  resourceID = utilrsw.get_path(dataset, 'info/resourceID', sep='/')
+
+  # Need to check citation
+  # If not DOI, need to find place for datasetCitation and resourceID in SPASE
+  if datasetCitation is not None:
+    DOI = extract_doi(datasetCitation)
+    if DOI is not None:
+      Spase['NumericalData']['ResourceHeader']['DOI'] = DOI
+  else:
+    if resourceID is not None:
+      DOI = extract_doi(resourceID)
+      if DOI is not None:
+        Spase['NumericalData']['ResourceHeader']['DOI'] = DOI
+      else:
+        desc = Spase['NumericalData']['ResourceHeader']['Description']
+        desc = desc.rstrip('.')
+        extra = f"HAPI resourceID: {resourceID}."
+        Spase['NumericalData']['ResourceHeader']['Description'] = f"{desc}. {extra}"
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 out_path = os.path.join(script_path, 'data', 'spase')
@@ -158,25 +281,32 @@ for server in servers:
 
   log.info(f"Processing server: {server}")
 
-  catalog = servers[server]['catalog']
-  about = servers[server]['about']
+  catalog = utilrsw.get_path(servers[server], 'catalog.catalog')
+  if catalog is None:
+    log.error(f"No catalog found for server: {server}")
+    continue
+
+  capabilities = utilrsw.get_path(servers[server], 'capabilities')
+  about = utilrsw.get_path(servers[server], 'about')
+
   for dataset in catalog:
 
     dataset['server'] = server
+    dataset['server_url'] = about['x_url']
     dataset['dataset'] = dataset['id']
 
     if reread_info:
       info_file = os.path.join(script_path, 'data', 'infos', server, f"{dataset['id']}.json")
       info_dict = utilrsw.read(info_file)
       dataset['info'] = info_dict
-      log.info(f"  Replacing info from {all_file} with that in {info_file}.")
+      log.info(f"  reread_info = True => overriding info from {all_file} with that in {info_file}.")
 
     #log.info("Input:\n" + utilrsw.format_dict(dataset, style='json'))
 
     add_NumericalData(Spase, dataset, config['hapi2spase']['dataset'])
-    add_DOI(Spase, dataset)
+    add_ResourceHeader(Spase, dataset)
     add_SpatialMapping(Spase, dataset)
-    add_AccessInformation(Spase, dataset, about, config['AccessInformation'])
+    add_AccessInformation(Spase, dataset, about, capabilities, config['formatMap'], config['AccessInformation'])
     add_Parameter(Spase, dataset, config['hapi2spase']['parameter'])
 
     #log.info("Output:\n" + utilrsw.format_dict(Spase, style='json'))
